2903ef9a98a04c01c02b381901d94da744f359fd
Paul MacDonald <paulrm6@gmail.com>
2016-03-03 14:30:05 +0000

    Update variable names

--- solution/intelligentweb.js
+++ solution/intelligentweb.js
@@ -1,6 +1,6 @@
 var protocol = require('http');
 var twitter = require('twit');
-var client = new Twit({
+var client = new twitter({
   consumer_key: '1R5gIb6Nq3Q0CSEaz37bRt2F9',
   consumer_secret: 'xZf90oSYBEBWvdxxzPcs3GBrh6r4XhuvaLF5s9xvTuLE0IW4pf',
   access_token: '705393120402382848-LUTIG8CQyn8jmr3OMhDsRuvf22AC0C0',
