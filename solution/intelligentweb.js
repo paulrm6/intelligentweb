6e3c1ce24038780c086af78073b5181afc52fe2d
Paul MacDonald <paulrm6@gmail.com>
2016-03-10 14:22:48 +0000

    Add Express

--- solution/intelligentweb.js
+++
@@ -1,8 +0,0 @@
-var protocol = require('http');
-var twitter = require('twit');
-var client = new twitter({
-  consumer_key: '1R5gIb6Nq3Q0CSEaz37bRt2F9',
-  consumer_secret: 'xZf90oSYBEBWvdxxzPcs3GBrh6r4XhuvaLF5s9xvTuLE0IW4pf',
-  access_token: '705393120402382848-LUTIG8CQyn8jmr3OMhDsRuvf22AC0C0',
-  access_token_secret: 'mEDTtSJtcRodO8FHBedwtAcqFTR9Vu16o93HzUrFP12L8'
-});
