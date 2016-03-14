8ce01af26942344767aedfce46cb226314650220
Paul MacDonald <paulrm6@gmail.com>
2016-03-14 15:26:39 +0000

    Add twitter config file

---
+++ solution/private/twit.js
@@ -0,0 +1,9 @@
+var Twit = require('twit');
+var client = new Twit({
+	consumer_key: '1R5gIb6Nq3Q0CSEaz37bRt2F9',
+	consumer_secret: 'xZf90oSYBEBWvdxxzPcs3GBrh6r4XhuvaLF5s9xvTuLE0IW4pf',
+	access_token: '705393120402382848-LUTIG8CQyn8jmr3OMhDsRuvf22AC0C0',
+	access_token_secret: 'mEDTtSJtcRodO8FHBedwtAcqFTR9Vu16o93HzUrFP12L8'
+	});
+
+module.exports = client;
