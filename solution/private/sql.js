d38ddab97374ba11621fa3262968c7d91c501c5c
Paul MacDonald <paulrm6@gmail.com>
2016-03-17 14:51:51 +0000

    Change to mysql

---
+++ solution/private/sql.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+	host		: 'stusql.dcs.shef.ac.uk',
+	port		: '3306',
+	user		: 'aca13prm',
+	password	: 'fd2f8cef',
+	database	: 'aca13prm',
+});
