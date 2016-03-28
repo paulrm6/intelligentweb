157f351668575f03443745461f6cefa116faf820
Paul MacDonald <paulrm6@gmail.com>
2016-03-28 23:38:59 +0100

    Fix the links

--- solution/routes/index.js
+++ solution/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var client = require('../private/twit');
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.sendfile('solution/views/index.html');
+	res.sendfile(path.join(__dirname, '../views', '/index.html'));
 });
 
 module.exports = router;
