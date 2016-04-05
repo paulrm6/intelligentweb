6a8e5fbcee1b83ca97f3bff5bd3231803a5fe531
Paul MacDonald <paulrm6@gmail.com>
2016-04-05 17:54:08 +0100

    link into database for team names

--- solution/app.js
+++ solution/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var search = require('./routes/search');
+var teaminfo = require('./routes/teaminfo');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/search', search);
+app.use('/teaminfo', teaminfo);
 app.use('/*', index);
 
 // catch 404 and forward to error handler
