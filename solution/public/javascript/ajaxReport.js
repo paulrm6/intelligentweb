395746dd7299a2e2b4370c64cd50a3ee40a7fb0d
Paul MacDonald <paulrm6@gmail.com>
2016-05-19 00:38:41 +0100

    Add links

--- solution/public/javascript/ajaxReport.js
+++ solution/public/javascript/ajaxReport.js
@@ -67,14 +67,20 @@ function setColour(data, team) {
 function populateReportData(data, team) {
 	var teamData = data[team].club.results.bindings[0];
 	setColour(teamData,team);
-	$('#report #results #'+team+' h3').text(teamData.fullname.value)
+	$('#report #results #'+team+' h3').html("<a href='"
+		+teamData['callret-0'].value
+		+"'>"+teamData.fullname.value+"</a>")
 	var teamInfo = "<div class='abstract readMore'>"+teamData.abstract.value+"</div>";
 	$('#report #results #'+team+' .teamInfo').empty().append(teamInfo)
-	var stadiumInfo = "<div class='stadiumName'>Stadium: "+teamData.groundName.value+"</div>"
+	var stadiumInfo = "<a href='"+
+		+teamData.ground.value
+		+"' class='stadiumName'>Stadium: "+teamData.groundName.value+"</a>"
 		+"<img src='"+teamData.groundThumbnail.value+"'/>"
 		+"<div class='abstract readMore'>"+teamData.groundDescription.value+"</div>";
 	$('#report #results #'+team+' .stadiumInfo').empty().append(stadiumInfo);
-	var managerInfo = "<div class='managerName'>Manager: "+teamData.managerName.value+"</div>"
+	var managerInfo = "<a href='"
+		+teamData.manager.value
+		+"' class='managerName'>Manager: "+teamData.managerName.value+"</a>"
 		+"<img src='"+teamData.managerThumbnail.value+"'/>";
 	$('#report #results #'+team+' .managerInfo').empty().append(managerInfo);
 	var playerInfo = ""
