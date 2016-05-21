6f4b0e2d12293113e303e158658d22e90dd91264
Paul MacDonald <paulrm6@gmail.com>
2016-05-21 18:44:07 +0100

    Changes to allow data to be missing

--- solution/public/javascript/ajaxReport.js
+++ solution/public/javascript/ajaxReport.js
@@ -70,39 +70,55 @@ function populateReportData(data, team) {
 	$('#report #results #'+team+' h3').html("<a target='_blank' href='"
 		+teamData['callret-0'].value
 		+" property='dbp:fullname'>"+teamData.fullname.value+"</a>");
-	var teamInfo = "<div class='abstract readMore' property='dbo:abstract'>"+teamData.abstract.value+"</div>"
-		+"<a property='dbo:ground' href='"+teamData.ground.value+"'></a>"
-		+"<a property='dbo:manager' href='"+teamData.manager.value+"'></a>";
-	var stadiumInfo = "<a target='_blank' href='"
-		+teamData.ground.value
-		+"' class='stadiumName'>Stadium: <span property='dbp:name'>"+teamData.groundName.value+"</span></a>"
-		+"<img src='"+teamData.groundThumbnail.value+"' property='dbo:thumbnail'/>"
-		+"<div class='abstract readMore' property='dbo:abstract'>"+teamData.groundDescription.value+"</div>";
-	var managerInfo = "<a target='_blank' href='"
-		+teamData.manager.value
-		+"' class='managerName'>Manager: <span property='dbp:name'>"+teamData.managerName.value+"</span></a>"
-		+"<img src='"+teamData.managerThumbnail.value+"' property='dbo:thumbnail' />";
-	var playerInfo = ""
+	var teamInfo="",stadiumInfo="",managerInfo="",playerInfo="";
+	if(teamData.abstract) {
+		teamInfo += "<div class='abstract readMore' property='dbo:abstract'>"+teamData.abstract.value+"</div>"
+		$('label[for=teamInfoReadMore_'+team+']').show();
+	} else {
+		$('label[for=teamInfoReadMore_'+team+']').hide();	
+	}
+	if(teamData.ground && teamData.groundName) {
+		stadiumInfo = "<a target='_blank' href='"
+			+teamData.ground.value
+			+"' class='stadiumName'>Stadium: <span property='dbp:name'>"+teamData.groundName.value+"</span></a>"
+			+"<img src='"
+			+((teamData.groundThumbnail)?teamData.groundThumbnail.value:"/images/generic_photo.png")
+			+"' property='dbo:thumbnail'/>"
+			+((teamData.groundDescription.value)?"<div class='abstract readMore' property='dbo:abstract'>"+teamData.groundDescription.value+"</div>":"");
+		teamInfo += "<a property='dbo:ground' href='"+teamData.ground.value+"'></a>";
+		$('#report #results #'+team+' .stadiumInfo').attr('about',teamData.ground.value);
+		$('label[for=stadiumInfoReadMore_'+team+']').show();
+	} else {
+		$('label[for=stadiumInfoReadMore_'+team+']').hide();	
+	}
+	if(teamData.manager && teamData.managerName) {
+		managerInfo = "<a target='_blank' href='"
+			+teamData.manager.value
+			+"' class='managerName'>Manager: <span property='dbp:name'>"+teamData.managerName.value+"</span></a>"
+			+"<img src='"
+			+((teamData.managerThumbnail)?teamData.managerThumbnail.value:"/images/generic_photo.png")
+			+"' property='dbo:thumbnail' />";
+		teamInfo += "<a property='dbo:manager' href='"+teamData.manager.value+"'></a>";
+		$('#report #results #'+team+' .managerInfo').attr('about',teamData.manager.value);
+	}
 	$.each(data[team].players.results.bindings, function(i, player) {
-		teamInfo += "<a property='dbp:name' href='"+player.player.value+"'></a>"
-		playerInfo += "<a target='_blank' href='"
-			+player.player.value
-			+"' class='player' about='"+player.player.value+"'><div class='playerName' property='dbp:name'>"
-			+player.playerName.value
-			+"</div><img src='"
-			+player.playerPhoto.value
-			+"' property='dbo:thumbnail'/><div class='playerPosition' property='dbo:position'>"
-			+player.playerPosition.value.replace(" (association football)","")
-			+"</div><div class='playerDOB'>DOB: <span property='dbp:birthDate'>"
-			+player.playerdob.value
-			+"</span></div></a>"
+		if(player.playerName) {
+			teamInfo += "<a property='dbp:name' href='"+player.player.value+"'></a>"
+			playerInfo += "<a target='_blank' href='"
+				+player.player.value
+				+"' class='player' about='"+player.player.value+"'>"
+				+"<div class='playerName' property='dbp:name'>"+player.playerName.value+"</div><img src='"
+				+((player.playerPhoto) ? player.playerPhoto.value : "/images/generic_photo.png")
+				+"' property='dbo:thumbnail'/>"
+				+((player.playerPosition) ? "<div class='playerPosition' property='dbo:position'>"+player.playerPosition.value.replace(" (association football)","")+"</div>" : "")
+				+((player.playerdob) ? "<div class='playerDOB'>DOB: <span property='dbp:birthDate'>"+player.playerdob.value+"</span></div>" : "")
+				+"</a>"
+		}
 	});
 	$('#report #results #'+team+' .teamInfo').empty().append(teamInfo)
 		.attr('about',teamData['callret-0'].value)
-	$('#report #results #'+team+' .stadiumInfo').empty().append(stadiumInfo)
-		.attr('about',teamData.ground.value);
-	$('#report #results #'+team+' .managerInfo').empty().append(managerInfo)
-		.attr('about',teamData.manager.value);
+	$('#report #results #'+team+' .stadiumInfo').empty().append(stadiumInfo);
+	$('#report #results #'+team+' .managerInfo').empty().append(managerInfo);
 	$('#report #results #'+team+' .playerInfo').empty().append(playerInfo);
 }
 
